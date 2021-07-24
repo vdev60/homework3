5c2c305432f473e727e2ec9845882fc27f303bc3
Dved <dved490@gmail.com>
2021-07-24 17:18:29 +0300

    Add FILE_lib_isascii.c

---
+++ FILE_lib_isascii.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "myhead.h"
+
+int lib_isascii(int c);
+bool compare_isascii(int a, int b);
+
+int main(){
+    char *str = "as24!f3*.!\n ";
+    bool compare_result;
+    int  i = 0;
+    while(str[i]){
+        compare_result = compare_isascii( isascii(str[i]), lib_isascii(str[i]) );
+        test_output(compare_result, i); 
+        i++;
+    } 
+
+    return 0;
+}
+
+int lib_isascii(int c){
+    if (c >= 0 && c <= 127){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+bool compare_isascii(int a, int b){
+    return a == b ? true : false ;
+}
+
